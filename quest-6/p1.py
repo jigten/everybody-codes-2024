236b694cc227d1a211ccd6c7d2b48e36dd49a52d
Ugyen Jigten Dorji <ujigten@gmail.com>
2026-08-11 16:05:26 +0600

    Quest 6 solutions

---
+++ quest-6/p1.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+input = open(0).read()
+adj = defaultdict(list)
+
+paths = defaultdict(list)
+
+for row in input.splitlines():
+    node, children = row.split(":")
+
+    for c in children.split(","):
+        adj[node].append(c)
+
+
+def dfs(node, path, path_len):
+    if node == "@":
+        paths[path_len].append(path)
+        return
+
+    for c in adj[node]:
+        if c in path:
+            continue
+        dfs(c, path + c, path_len + 1)
+
+
+dfs("RR", "RR", 1)
+
+for path in paths.values():
+    if len(path) == 1:
+        print(f"Answer: {path[0]}")
